3ee37cf3139ebe02832a57671af3d04e32432f84
Jessy <jessy.etokabeka@gmail.com>
2021-11-13 16:12:35 +0100

    remove authent check on get detail of an offer

--- routes/offer-routes.js
+++ routes/offer-routes.js
@@ -149,7 +149,7 @@ router.get('/offers', async (req, res) => {
 });
 
 //Get detail on a specific id
-router.get('/offer/:id', isAuthenticated, async (req, res) => {
+router.get('/offer/:id', async (req, res) => {
   try {
     const offer = await Offer.findById(req.params.id)
       .populate({ path: 'owner', select: 'account' })
